580bade5a593200ea35fcad57b6ab9937e24a6a2
AJ Javadi <amirjavadi25@gmail.com>
2025-01-08 21:21:03 -0600

    Add relationships and fillable properties to Comment and Post models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    // fillable
+    protected $fillable = [
+        'content',
+        'user_id',
+        'post_id'
+    ];
+
+    // relationship to User
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    // relationship to Posts
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    // fillable
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id'
+    ];
+
+    // relationship to User Model
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    // relationship to Comment Model
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
