df58cd944da92adf3e02024ca992dcf53882362a
AJ Javadi <amirjavadi25@gmail.com>
2025-01-09 22:46:35 -0600

    Add seed data script and update notes with TODOs for Laravel Vue Inertia CMS setup

---
+++ zzAJ/seed_data.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+** AJ Javadi
+** Created 1/9/2025
+** /Users/aj/Herd/laravel-newsfeed/zzAJ/seed_data.php
+*/
+
+
+$testUser1 = [
+    3,
+    'zztest1',
+    'zztest1@example.com',
+    NULL,
+    '$2y$12$CZN16M7EmaiTprMxerYj8OovH.Oix2fToAAyYjQLH5ffT4naQRA5q',
+    NULL,
+    '2025-01-09 16:46:03',
+    '2025-01-09 16:46:03'
+];
+
+var_dump($testUser1);
